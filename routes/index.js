5b876938adb4090cd09c5389c9ded2bed70cdb0e
Jeffrey Farber <jeff@transparentelectric.com>
2016-09-08 10:26:56 -0700

    Customize styling/templates for lesson

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    bodytext: 'Hello Full-Stack Class, you\'ve successfully loaded your first webpage!'
+  });
 });
 
 module.exports = router;
